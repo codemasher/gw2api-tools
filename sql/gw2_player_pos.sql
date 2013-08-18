13283e3ca3882b618ba80acf60aadde8d9fded13
smiley <smiley@chillerlan.net>
2013-08-18 02:10:16 +0200

    added commander and team_color columns

--- sql/gw2_player_pos.sql
+++ sql/gw2_player_pos.sql
@@ -4,6 +4,8 @@ CREATE TABLE IF NOT EXISTS `gw2_player_pos` (
   `acc_name` varchar(40) COLLATE utf8_bin NOT NULL,
   `char_name` varchar(20) COLLATE utf8_bin NOT NULL,
   `profession` tinyint(2) unsigned NOT NULL,
+  `team_color` tinyint(1) unsigned NOT NULL,
+  `commander` tinyint(1) unsigned NOT NULL,
   `guild_id` varchar(36) COLLATE utf8_bin NOT NULL,
   `guild_secret` varchar(40) COLLATE utf8_bin NOT NULL,
   `world_id` smallint(4) unsigned NOT NULL,
@@ -15,5 +17,5 @@ CREATE TABLE IF NOT EXISTS `gw2_player_pos` (
   PRIMARY KEY (`player_uid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
-INSERT INTO `gw2_player_pos` (`player_uid`, `acc_name`, `char_name`, `profession`, `guild_id`, `guild_secret`, `world_id`, `map_id`, `pos_x`, `pos_y`, `pos_angle`, `pos_time`) VALUES
-('54978e6a09a2588f2bc3017dd8dcf8c0417eaab6', 'smiley.1438', 'Skin Receiver', 4, '75FD83CF-0C45-4834-BC4C-097F93A487AF', '76cbfee2dd33d5d34bfecc274ea5e766e11ead92', 2005, 50, -3254.7300, 11333.9004, 4, 1373293820);
+INSERT INTO `gw2_player_pos` (`player_uid`, `acc_name`, `char_name`, `profession`, `team_color`, `commander`, `guild_id`, `guild_secret`, `world_id`, `map_id`, `pos_x`, `pos_y`, `pos_angle`, `pos_time`)
+VALUES ('54978e6a09a2588f2bc3017dd8dcf8c0417eaab6', 'smiley.1438', 'Skin Receiver', 6, 0, 0, '75FD83CF-0C45-4834-BC4C-097F93A487AF', '76cbfee2dd33d5d34bfecc274ea5e766e11ead92', 2005, 50, -1677.2900, 11278.7002, 296, 1376011288);
