5a47dbea4cf51034f43c443474ecb6779bb25757
smiley <smiley@chillerlan.net>
2013-09-28 13:16:46 +0200

    ah right, you need these 2 - for maps.sql see my database repo

--- sql/gw2_player_pos.sql
+++ sql/gw2_player_pos.sql
@@ -1,21 +1,25 @@
-DROP TABLE IF EXISTS `gw2_player_pos`;
 CREATE TABLE IF NOT EXISTS `gw2_player_pos` (
-  `player_uid` varchar(40) COLLATE utf8_bin NOT NULL,
-  `acc_name` varchar(40) COLLATE utf8_bin NOT NULL,
-  `char_name` varchar(20) COLLATE utf8_bin NOT NULL,
-  `profession` tinyint(2) unsigned NOT NULL,
-  `team_color` tinyint(1) unsigned NOT NULL,
-  `commander` tinyint(1) unsigned NOT NULL,
-  `guild_id` varchar(36) COLLATE utf8_bin NOT NULL,
-  `guild_secret` varchar(40) COLLATE utf8_bin NOT NULL,
-  `world_id` smallint(4) unsigned NOT NULL,
-  `map_id` smallint(4) unsigned NOT NULL,
-  `pos_x` float(10,4) NOT NULL,
-  `pos_y` float(10,4) NOT NULL,
-  `pos_angle` smallint(3) unsigned NOT NULL,
-  `pos_time` int(10) unsigned NOT NULL,
+  `player_uid`   VARCHAR(40)
+                 COLLATE utf8_bin     NOT NULL,
+  `acc_name`     VARCHAR(40)
+                 COLLATE utf8_bin     NOT NULL,
+  `char_name`    VARCHAR(20)
+                 COLLATE utf8_bin     NOT NULL,
+  `profession`   TINYINT(2) UNSIGNED  NOT NULL,
+  `team_color`   SMALLINT(4) UNSIGNED NOT NULL,
+  `commander`    TINYINT(1) UNSIGNED  NOT NULL,
+  `guild_id`     VARCHAR(36)
+                 COLLATE utf8_bin     NOT NULL,
+  `guild_secret` VARCHAR(40)
+                 COLLATE utf8_bin     NOT NULL,
+  `world_id`     SMALLINT(4) UNSIGNED NOT NULL,
+  `map_id`       SMALLINT(4) UNSIGNED NOT NULL,
+  `pos_x`        FLOAT(10, 4)         NOT NULL,
+  `pos_y`        FLOAT(10, 4)         NOT NULL,
+  `pos_angle`    SMALLINT(3) UNSIGNED NOT NULL,
+  `pos_time`     INT(10) UNSIGNED     NOT NULL,
   PRIMARY KEY (`player_uid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
-INSERT INTO `gw2_player_pos` (`player_uid`, `acc_name`, `char_name`, `profession`, `team_color`, `commander`, `guild_id`, `guild_secret`, `world_id`, `map_id`, `pos_x`, `pos_y`, `pos_angle`, `pos_time`)
-VALUES ('54978e6a09a2588f2bc3017dd8dcf8c0417eaab6', 'smiley.1438', 'Skin Receiver', 6, 0, 0, '75FD83CF-0C45-4834-BC4C-097F93A487AF', '76cbfee2dd33d5d34bfecc274ea5e766e11ead92', 2005, 50, -1677.2900, 11278.7002, 296, 1376011288);
+)
+  ENGINE =InnoDB
+  DEFAULT CHARSET =utf8
+  COLLATE =utf8_bin;
