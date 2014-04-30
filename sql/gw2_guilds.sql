322e656b8b5fc28f37114397e8ffa08b9867124f
smiley <smiley@chillerlan.net>
2014-05-01 01:57:06 +0200

    re-added and fixed needed tabled for the location tracker

--- sql/gw2_guilds.sql
+++ sql/gw2_guilds.sql
@@ -1,14 +1,8 @@
 CREATE TABLE IF NOT EXISTS `gw2_guilds` (
-  `guild_id` VARCHAR(40)
-             COLLATE utf8_bin NOT NULL,
-  `name`     TINYTEXT
-             COLLATE utf8_bin NOT NULL,
-  `tag`      VARCHAR(4)
-             COLLATE utf8_bin NOT NULL,
-  `emblem`   TINYTEXT
-             COLLATE utf8_bin NOT NULL,
+  `guild_id` VARCHAR(40) COLLATE utf8_bin NOT NULL,
+  `name` TINYTEXT COLLATE utf8_bin NOT NULL,
+  `tag` VARCHAR(4) COLLATE utf8_bin NOT NULL,
+  `emblem` TINYTEXT COLLATE utf8_bin NOT NULL,
   PRIMARY KEY (`guild_id`)
 )
-  ENGINE =InnoDB
-  DEFAULT CHARSET =utf8
-  COLLATE =utf8_bin;
+ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
--- sql/gw2_player_pos.sql
+++ sql/gw2_player_pos.sql
@@ -1,25 +1,17 @@
 CREATE TABLE IF NOT EXISTS `gw2_player_pos` (
-  `player_uid`   VARCHAR(40)
-                 COLLATE utf8_bin     NOT NULL,
-  `acc_name`     VARCHAR(40)
-                 COLLATE utf8_bin     NOT NULL,
-  `char_name`    VARCHAR(20)
-                 COLLATE utf8_bin     NOT NULL,
-  `profession`   TINYINT(2) UNSIGNED  NOT NULL,
-  `team_color`   SMALLINT(4) UNSIGNED NOT NULL,
-  `commander`    TINYINT(1) UNSIGNED  NOT NULL,
-  `guild_id`     VARCHAR(36)
-                 COLLATE utf8_bin     NOT NULL,
-  `guild_secret` VARCHAR(40)
-                 COLLATE utf8_bin     NOT NULL,
-  `world_id`     SMALLINT(4) UNSIGNED NOT NULL,
-  `map_id`       SMALLINT(4) UNSIGNED NOT NULL,
-  `pos_x`        FLOAT(10, 4)         NOT NULL,
-  `pos_y`        FLOAT(10, 4)         NOT NULL,
-  `pos_angle`    SMALLINT(3) UNSIGNED NOT NULL,
-  `pos_time`     INT(10) UNSIGNED     NOT NULL,
+  `player_uid` varchar(40) COLLATE utf8_bin NOT NULL,
+  `acc_name` varchar(40) COLLATE utf8_bin NOT NULL,
+  `char_name` varchar(20) COLLATE utf8_bin NOT NULL,
+  `profession` tinyint(2) unsigned NOT NULL,
+  `team_color` smallint(4) unsigned NOT NULL,
+  `commander` tinyint(1) unsigned NOT NULL,
+  `guild_id` varchar(36) COLLATE utf8_bin NOT NULL,
+  `guild_secret` varchar(40) COLLATE utf8_bin NOT NULL,
+  `world_id` smallint(4) unsigned NOT NULL,
+  `map_id` smallint(4) unsigned NOT NULL,
+  `pos_x` float(10,4) NOT NULL,
+  `pos_y` float(10,4) NOT NULL,
+  `pos_angle` smallint(3) unsigned NOT NULL,
+  `pos_time` int(10) unsigned NOT NULL,
   PRIMARY KEY (`player_uid`)
-)
-  ENGINE =InnoDB
-  DEFAULT CHARSET =utf8
-  COLLATE =utf8_bin;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
